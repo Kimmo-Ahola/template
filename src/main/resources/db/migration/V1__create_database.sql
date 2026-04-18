2018173d5a84f442de32ff828c893d287a96307e
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2026-04-18 17:16:31 +0200

    added sql scripts

---
+++ src/main/resources/db/migration/V1__create_database.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS my_cloud_db;
+USE my_cloud_db;
---
+++ src/main/resources/db/migration/V2__create_author_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE author (
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255)  NOT NULL
+);
